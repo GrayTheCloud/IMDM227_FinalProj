cfd54d0afbb3ea1196916d7b4cba28ace4856a82
Gray <155936239+GrayTheCloud@users.noreply.github.com>
2025-11-25 16:48:33 -0500

    made a sample scene to size

---
+++ IMDM 227 Final Project/Assets/Code/StoryScene.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryScene : MonoBehaviour
+{
+    public StoryScene next1;
+    public StoryScene next2;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

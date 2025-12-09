8e2c43f33d6d23aa6bc07d80855ec12e79428559
AngelaBorinski <Angborinski@gmail.com>
2025-12-09 17:02:15 -0500

    wrote materialChanger class

--- IMDM 227 Final Project/Assets/Code/MyButton.cs
+++ IMDM 227 Final Project/Assets/Code/MyButton.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class MyButton : MonoBehaviour, IClickable
 {
     public int choiceNum;
+
     public void Click()
     {
         Debug.Log("I am a button and I've been clicked");
+
+        
     }
 
 }
